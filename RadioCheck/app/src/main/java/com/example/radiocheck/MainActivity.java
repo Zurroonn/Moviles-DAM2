1e07562f0351edcdf7c9241bcbea3f222db30c63
Alejandro Zurrón Pérez <aazurron@gmail.com>
2024-10-08 09:43:33 +0200

    commit

--- RadioCheck/app/src/main/java/com/example/radiocheck/MainActivity.java
+++ RadioCheck/app/src/main/java/com/example/radiocheck/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.radiocheck;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
@@ -14,91 +16,93 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    // Declaramos las variables para los elementos de la interfaz
-    EditText editText;  // Donde se muestra el texto que cambia
-    RadioGroup radioGroupColorFondo, radioGroupColorFuente;  // RadioGroups para color de fondo y color de fuente
-    CheckBox checkBoxNegrita, checkBoxCursiva, checkBoxMonospace, checkBoxSerif;  // Checkboxes para el tipo de letra
+    CheckBox cb1, cb2, cb3, cb4;
+    TextView textView;
+    RadioGroup rg1, rg2;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Vinculamos los elementos del layout con las variables de Java
-        editText = findViewById(R.id.editTextText);  // El texto que va a cambiar
-        radioGroupColorFondo = findViewById(R.id.radioGroup);  // RadioGroup de color de fondo
-        radioGroupColorFuente = findViewById(R.id.radioGroup2);  // RadioGroup de color de fuente (Corregido con ID diferente)
-        checkBoxNegrita = findViewById(R.id.checkBox);  // CheckBox para negrita
-        checkBoxCursiva = findViewById(R.id.checkBox5);  // CheckBox para cursiva
-        checkBoxMonospace = findViewById(R.id.checkBox3);  // CheckBox para Monospace
-        checkBoxSerif = findViewById(R.id.checkBox4);  // CheckBox para Serif
-
-        // Cambia el color de fondo según el botón de radio seleccionado
-        radioGroupColorFondo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // Usamos if-else para comprobar qué botón de radio fue seleccionado
-                if (checkedId == R.id.radioButton) {  // Fondo rojo
-                    editText.setBackgroundColor(Color.RED);
-                } else if (checkedId == R.id.radioButton2) {  // Fondo azul
-                    editText.setBackgroundColor(Color.BLUE);
-                } else if (checkedId == R.id.radioButton3) {  // Fondo verde
-                    editText.setBackgroundColor(Color.GREEN);
-                } else if (checkedId == R.id.radioButton4) {  // Fondo blanco
-                    editText.setBackgroundColor(Color.WHITE);
-                } else if (checkedId == R.id.radioButton5) {  // Fondo negro
-                    editText.setBackgroundColor(Color.BLACK);
-                }
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
 
-        // Cambia el color de la fuente según el botón de radio seleccionado
-        radioGroupColorFuente.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // Usamos if-else para verificar el color de la fuente
-                if (checkedId == R.id.radioButton6) {  // Fuente roja
-                    editText.setTextColor(Color.RED);
-                } else if (checkedId == R.id.radioButton7) {  // Fuente azul
-                    editText.setTextColor(Color.BLUE);
-                } else if (checkedId == R.id.radioButton8) {  // Fuente verde
-                    editText.setTextColor(Color.GREEN);
-                } else if (checkedId == R.id.radioButton9) {  // Fuente blanca
-                    editText.setTextColor(Color.WHITE);
-                } else if (checkedId == R.id.radioButton10) {  // Fuente negra
-                    editText.setTextColor(Color.BLACK);
-                }
-            }
-        });
+        // Inicializar CheckBoxes y RadioButtons
+        cb1 = findViewById(R.id.checkBox); // Negrita
+        cb2 = findViewById(R.id.checkBox2); // Cursiva
+        cb3 = findViewById(R.id.checkBox3); // Serif
+        cb4 = findViewById(R.id.checkBox4); // MonoSpace
 
-        // Cambia el tipo de letra según los checkboxes seleccionados
-        CheckBox.OnCheckedChangeListener tipoDeLetraListener = (buttonView, isChecked) -> {
-            // Estilo de letra: negrita y/o cursiva
-            int estilo = Typeface.NORMAL;
-            if (checkBoxNegrita.isChecked() && checkBoxCursiva.isChecked()) {
-                estilo = Typeface.BOLD_ITALIC;
-            } else if (checkBoxNegrita.isChecked()) {
-                estilo = Typeface.BOLD;
-            } else if (checkBoxCursiva.isChecked()) {
-                estilo = Typeface.ITALIC;
-            }
+        textView = findViewById(R.id.textView);
 
-            // Cambiamos la familia de la fuente (monospace o serif)
-            if (checkBoxMonospace.isChecked()) {
-                editText.setTypeface(Typeface.MONOSPACE, estilo);
-            } else if (checkBoxSerif.isChecked()) {
-                editText.setTypeface(Typeface.SERIF, estilo);
-            } else {
-                editText.setTypeface(Typeface.SANS_SERIF, estilo);  // Fuente predeterminada
-            }
-        };
+        rg1 = findViewById(R.id.radioGroup); // Color de fondo
+        rg2 = findViewById(R.id.radioGroup2); // Color de fuente
 
-        // Aplicamos el listener de tipo de letra a los checkboxes
-        checkBoxNegrita.setOnCheckedChangeListener(tipoDeLetraListener);
-        checkBoxCursiva.setOnCheckedChangeListener(tipoDeLetraListener);
-        checkBoxMonospace.setOnCheckedChangeListener(tipoDeLetraListener);
-        checkBoxSerif.setOnCheckedChangeListener(tipoDeLetraListener);
+        // Escuchar cambios en los CheckBoxes para el tipo de letra
+        cb1.setOnCheckedChangeListener((buttonView, isChecked) -> setTypeface());
+        cb2.setOnCheckedChangeListener((buttonView, isChecked) -> setTypeface());
+        cb3.setOnCheckedChangeListener((buttonView, isChecked) -> setTypeface());
+        cb4.setOnCheckedChangeListener((buttonView, isChecked) -> setTypeface());
+
+        // Escuchar cambios en los RadioGroups para cambiar color de fondo y fuente
+        rg1.setOnCheckedChangeListener((group, checkedId) -> setBackgroundColor(checkedId));
+        rg2.setOnCheckedChangeListener((group, checkedId) -> setTextColor(checkedId));
+    }
+
+    // Método para configurar el tipo de letra según los CheckBoxes
+    private void setTypeface() {
+        int style = Typeface.NORMAL;
+
+        if (cb1.isChecked() && cb2.isChecked()) {
+            style = Typeface.BOLD_ITALIC;
+        } else if (cb1.isChecked()) {
+            style = Typeface.BOLD;
+        } else if (cb2.isChecked()) {
+            style = Typeface.ITALIC;
+        }
+
+        if (cb3.isChecked()) {
+            textView.setTypeface(Typeface.SERIF, style);
+        } else if (cb4.isChecked()) {
+            textView.setTypeface(Typeface.MONOSPACE, style);
+        } else {
+            textView.setTypeface(Typeface.SANS_SERIF, style);
+        }
+    }
+
+    // Método para configurar el color de fondo utilizando if-else if
+    private void setBackgroundColor(int checkedId) {
+        if (checkedId == R.id.radioButton6) { // Rojo
+            textView.setBackgroundColor(Color.RED);
+        } else if (checkedId == R.id.radioButton7) { // Azul
+            textView.setBackgroundColor(Color.BLUE);
+        } else if (checkedId == R.id.radioButton8) { // Verde
+            textView.setBackgroundColor(Color.GREEN);
+        } else if (checkedId == R.id.radioButton10) { // Blanco
+            textView.setBackgroundColor(Color.WHITE);
+        } else if (checkedId == R.id.radioButton9) { // Negro
+            textView.setBackgroundColor(Color.BLACK);
+        }
+    }
+
+    // Método para configurar el color de fuente utilizando if-else if
+    private void setTextColor(int checkedId) {
+        if (checkedId == R.id.radioButton) { // Rojo
+            textView.setTextColor(Color.RED);
+        } else if (checkedId == R.id.radioButton2) { // Azul
+            textView.setTextColor(Color.BLUE);
+        } else if (checkedId == R.id.radioButton3) { // Verde
+            textView.setTextColor(Color.GREEN);
+        } else if (checkedId == R.id.radioButton5) { // Blanco
+            textView.setTextColor(Color.WHITE);
+        } else if (checkedId == R.id.radioButton4) { // Negro
+            textView.setTextColor(Color.BLACK);
+        }
     }
 }
-
