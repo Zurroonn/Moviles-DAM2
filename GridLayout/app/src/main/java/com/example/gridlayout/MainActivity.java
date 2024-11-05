988d810643b9fe9bc6dfa1a10ea53ec8d1708526
Alejandro Zurrón Pérez <aazurron@gmail.com>
2024-11-05 14:17:29 +0100

    commit

--- GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.java
+++ GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -5,8 +5,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -17,6 +15,7 @@ import androidx.gridlayout.widget.GridLayout;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 GridLayout gl;
     @SuppressLint("MissingInflatedId")
     @Override
